29df8fa5e68d92122a9c81f0f95aec16507a81be
Nikhilbhati007 <nikhilbhati1827ai@gmail.com>
2026-07-13 18:06:44 +0530

    commit

--- Javascript/function..js
+++ Javascript/function..js
@@ -1,9 +1,20 @@
 
-function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(2, 3)); // Output: 5
+// function say_my_name(name) {    
+//     console.log("My name is " + name);
+// }
+// say_my_name("Nikhil Bhati"); // Output: My name is John
+//++++++++++++++++++++++Arrow Function++++++++++++++++++++++
+const add = (a, b) => a + b;
 console.log(add(2, 3)); // Output: 5
-function say_my_name(name) {    
-    console.log("My name is " + name);
+
+//++++++++++++++++++++++++++THIS Function++++++++++++++++++++++++++
+console.log(this); // Output: Window or {}
+function say_my_name(name) {
+    let my_name=name;
+    console.log("My name is " + this.my_name);
 }
-say_my_name("Nikhil Bhati"); // Output: My name is John
+say_my_name("Nikhil Bhati"); // Output: My name is undefined
